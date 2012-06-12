9c30ed73fa79711f360a724f0e0b2c63f89364e3
unknown <user@user-PC.(none)>
2012-06-12 14:32:59 +0530

    "modified frontend page"

---
+++ includes/src/Excellence2_Profile_Block_Profile.php
@@ -0,0 +1,4 @@
+<?php
+class Excellence2_Profile_Block_Profile extends Mage_Core_Block_Template
+{
+}
---
+++ includes/src/Excellence2_Profile_Helper_Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Excellence2_Profile_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
